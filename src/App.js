02785b75d47397b9cc12043da5cf918781549929
unknown <shuvo616@gmail.com>
2021-02-25 16:57:58 +0600

    Create Component, return multiple HTML from a component and learn how to style the element

--- src/App.js
+++ src/App.js
@@ -7,9 +7,26 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Module 32: Explore React !!!</h1>
+        <Person></Person>
+        <Person></Person>
       </header>
     </div>
   );
 }
+
+// Declare first component
+function Person(){
+  const personStyle = {
+    border: '2px solid red',
+    margin: '10px',
+    padding: '10px'
+  }
+  return (
+    <div style={personStyle}>
+      <h1 style={{color: 'green'}}>First Component</h1>
+      <p>Learning to declare component</p>
+    </div>
+  )
+} 
 
 export default App;
