b720982c01c505561c9ddb636950dc3f2d32fedc
unknown <shuvo616@gmail.com>
2021-02-26 03:29:51 +0600

    Explore State and  useEffect

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  const products =[
+  const bike = [
+    {bikeName: 'Yamaha', bikePrice: '$10'},
+    {bikeName: 'Pulsar', bikePrice: '$20'},
+    {bikeName: 'Suzuki', bikePrice: '$30'},
+    {bikeName: 'Hero', bikePrice: '$40'}
+  ]
+  const products = [
     {name: 'Audi', price: '$100'},
     {name: 'BMW' , price: '$200'},
     {name: 'Prado', price: '$300'}
@@ -12,9 +19,19 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Module 32: Explore React !!!</h1>
-        <Product car={products[0]}></Product>
-        <Product car={products[1]}></Product>
-        <Product car={products[2]}></Product>
+        {/* Counter component for understanding State */}
+        <Counter></Counter>
+        <Users></Users>
+        {
+          products.map(pd=> <Product product={pd}></Product>)
+        }
+        {/* <Product product={products[0]}></Product>
+        <Product product={products[1]}></Product>
+        <Product product={products[2]}></Product> */}
+        {
+          bike.map(bk=> <Rider newBike={bk}></Rider>)
+        }
+        {/* <Rider bike={bike[0]}></Rider> */}
         <Person name="John Wick" surname="Baba Yaga"></Person>
         <Person name="Dwayne Johnson" surname="Rock"></Person>
       </header>
@@ -33,7 +50,7 @@ function Product(props){
     margin: '10px'
   }
   // object destructuring as a variable
-  const {name,price} = props.car;
+  const {name,price} = props.product;
   return (
     <div style={productStyle}>
       <h2>Car: {name}</h2>
@@ -42,7 +59,63 @@ function Product(props){
     </div>
   )
 }
+// Component for Bike
+function Rider(props){
+  const bikeStyle = {
+    backgroundColor: 'green',
+    border: '1px solid red',
+    borderRadius: '10px',
+    margin: '10px',
+    height: '150px',
+    width: '300px',
+    float:'left'
+  }
+    // const {newName,newPrice} = props.newBike;
+  return(
+    <div style={bikeStyle}>
+      <h3>Bike Name: {props.newBike.bikeName}</h3>
+      <p>Price: {props.newBike.bikePrice}</p>
+    </div>
+  )
+}
+// State
+function Counter(){
+  const [count,setCount] = useState(1);
+  // const handelIncrease = () => {
+  //   const newCount = count + 1;
+  //   setCount(newCount);
+  // };
+   const handelIncrease = () => setCount(count + 1);
+  return(
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+    </div>
+  )
+}
 
+// 32-12 Load dynamic data, API call useEffect integrate state
+function Users(){
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  }, [])
+
+  return(
+    <div>
+      <h2>Dynamic Users:{users.length} </h2>
+      <ul>
+        {
+          users.map(user => <li>{(user.name)}</li>)
+        }
+      </ul>
+    </div>
+  )
+}
 // Declare first component
 function Person(props){
   const personStyle = {
