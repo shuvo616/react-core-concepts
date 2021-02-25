1a488e4d01e0cac71af74efaad5f00c865e34147
unknown <shuvo616@gmail.com>
2021-02-25 18:54:28 +0600

    Pass object to components and access object

--- src/App.js
+++ src/App.js
@@ -2,16 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const products =[
+    {name: 'Audi', price: '$100'},
+    {name: 'BMW' , price: '$200'},
+    {name: 'Prado', price: '$300'}
+  ]
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>Module 32: Explore React !!!</h1>
+        <Product car={products[0]}></Product>
+        <Product car={products[1]}></Product>
+        <Product car={products[2]}></Product>
         <Person name="John Wick" surname="Baba Yaga"></Person>
         <Person name="Dwayne Johnson" surname="Rock"></Person>
       </header>
     </div>
   );
+}
+// Create Second Component to pass Object and access the object
+function Product(props){
+  const productStyle = {
+    backgroundColor: 'lightgray',
+    border: '1px solid red',
+    borderRadius: '10px',
+    height: '200px',
+    width: '200px',
+    float: 'left',
+    margin: '10px'
+  }
+  // object destructuring as a variable
+  const {name,price} = props.car;
+  return (
+    <div style={productStyle}>
+      <h2>Car: {name}</h2>
+      <h3>Price: {price}</h3>
+      <button>Buy Now</button>
+    </div>
+  )
 }
 
 // Declare first component
@@ -21,7 +50,6 @@ function Person(props){
     margin: '10px',
     padding: '10px'
   }
-  console.log(props);
   return (
     <div style={personStyle}>
       <h1 style={{color: 'green'}}>First Component</h1>
